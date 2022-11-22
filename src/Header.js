fe9e24c74425a85adc1f4b78f31f6422afcbb5ee
Aigul <sharipova.aig@gmail.com>
2022-11-21 21:08:04 -0500

    header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function HeaderList() {
+  return (
+    <div className="header">
+      <h3>natgeo </h3>
+      <button className="oneButton">Follow</button>
+      <button className="twoButton">Message</button>
+      <button className="threeButton">A</button>
+      <button className="fourButton">B</button>
+    </div>
+  )
+}
+
+export default HeaderList
